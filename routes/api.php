a596f9ffd764f0695cf480b0650cf9e110c5baba
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-23 03:00:44 +0300

    feat: add refresh token route

--- routes/api.php
+++ routes/api.php
@@ -25,13 +25,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
 
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware(['auth:sanctum', 'access-token'])->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
     });
 });
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'access-token'])->group(function () {
     Route::prefix('v1/me')->group(function () {
         Route::get('/', ProfileController::class);
         Route::patch('/profile', [ProfileController::class, 'update']);
