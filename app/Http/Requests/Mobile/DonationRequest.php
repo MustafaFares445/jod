c95e9388f23c35e22abf0b468a5dc9cc5740d102
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-16 01:45:29 +0300

    Add mobile donation validation

---
+++ app/Http/Requests/Mobile/DonationRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Mobile;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class DonationRequest extends FormRequest
+{
+    /**
+     * @return array<string, list<mixed>>
+     */
+    public function rules(): array
+    {
+        return [
+            'amount' => ['required', 'numeric', 'decimal:0,2', 'min:0.01', 'max:999999999.99'],
+            'paymentMethod' => ['required', 'string', Rule::in(['credit_card', 'bank_transfer', 'cash', 'other'])],
+            'phone' => ['nullable', 'string', 'max:20'],
+            'city' => ['nullable', 'string', 'max:100'],
+        ];
+    }
+}
