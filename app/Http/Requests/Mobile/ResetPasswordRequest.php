c7e1a16a718416abde8e6d7175700dd2d4c0be52
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-16 10:40:49 +0300

    align reset password request

--- app/Http/Requests/Mobile/ResetPasswordRequest.php
+++ app/Http/Requests/Mobile/ResetPasswordRequest.php
@@ -13,11 +13,23 @@ class ResetPasswordRequest extends FormRequest
         return true;
     }
 
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'login' => $this->input('login', $this->input('phoneNumber')),
+            'password' => $this->input('password', $this->input('newPassword')),
+            'password_confirmation' => $this->input(
+                'password_confirmation',
+                $this->input('confirmPassword'),
+            ),
+        ]);
+    }
+
     public function rules(): array
     {
         return [
             'login' => ['required', 'string', 'max:255'],
-            'code' => ['required', 'string', 'size:6'],
+            'code' => ['required', 'string', 'size:4', 'regex:/^\d{4}$/'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ];
     }
