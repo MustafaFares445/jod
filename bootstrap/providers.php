8eb3b671021698a422a50cb861b40c5f75d4f4db
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-16 11:03:36 +0300

    register mobile event provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,12 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\AuthServiceProvider;
+use App\Providers\MobileEventServiceProvider;
 use App\Providers\MobilePushServiceProvider;
 
 return [
     AppServiceProvider::class,
     AuthServiceProvider::class,
     MobilePushServiceProvider::class,
+    MobileEventServiceProvider::class,
 ];
