2e216bf68c42d4823f17c205f42ab01f4d3055e8
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-24 17:52:19 +0300

    Remove slug from article input data

--- app/Data/ArticleData.php
+++ app/Data/ArticleData.php
@@ -5,17 +5,15 @@ declare(strict_types=1);
 namespace App\Data;
 
 use Carbon\Carbon;
-use Spatie\LaravelData\Data;
-use Spatie\LaravelData\Attributes\Validation\Max;
 use Spatie\LaravelData\Attributes\Validation\In;
+use Spatie\LaravelData\Attributes\Validation\Max;
+use Spatie\LaravelData\Data;
 
 class ArticleData extends Data
 {
     public function __construct(
         #[Max(255)]
         public ?string $title = null,
-        #[Max(255)]
-        public ?string $slug = null,
         #[Max(500)]
         public ?string $excerpt = null,
         public ?string $content = null,
@@ -30,7 +28,6 @@ class ArticleData extends Data
     {
         return array_filter([
             'title' => $this->title,
-            'slug' => $this->slug,
             'excerpt' => $this->excerpt,
             'content' => $this->content,
             'status' => $this->status,
