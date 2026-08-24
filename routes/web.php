3bdd1ab97019bcea0a4c38733a335b091a79fd1c
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-24 16:55:01 +0300

    Add Firebase browser test route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/firebase/push-test', function () {
+    abort_unless((bool) config('mobile_push.test_endpoint_enabled'), 404);
+
+    return view('firebase-push-test');
+})->name('firebase.push-test');
