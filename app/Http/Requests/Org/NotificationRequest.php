5257f26c1696a447a85b5b0f49399ae32c7240bc
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-27 02:44:41 +0300

    Allow help notification category

--- app/Http/Requests/Org/NotificationRequest.php
+++ app/Http/Requests/Org/NotificationRequest.php
@@ -16,7 +16,7 @@ class NotificationRequest extends FormRequest
         return [
             'title' => ['required', 'string', 'max:255'],
             'body' => ['required', 'string'],
-            'category' => ['required', Rule::in(['campaign', 'post', 'account', 'report', 'system', 'donation', 'applicant', 'staff'])],
+            'category' => ['required', Rule::in(['campaign', 'post', 'account', 'report', 'system', 'donation', 'help', 'applicant', 'staff'])],
             'eventType' => ['nullable', Rule::enum(NotificationEventType::class)],
             'recipientScope' => ['nullable', Rule::in(['all', 'users', 'organizations'])],
             'recipientLabel' => ['nullable', 'string', 'max:255'],
