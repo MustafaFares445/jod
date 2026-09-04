f0dfc3815216dc391fc5e9a1bbe80b7b027c934f
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-04 15:01:29 +0300

    feat(recommendations): expose exploration impression flag

--- app/Models/RecommendationImpression.php
+++ app/Models/RecommendationImpression.php
@@ -10,16 +10,26 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['id', 'user_id', 'subject_type', 'subject_id', 'feed_type', 'category_id', 'publisher_type', 'publisher_id', 'city', 'score', 'reasons', 'shown_at'])]
+#[Fillable([
+    'id', 'user_id', 'subject_type', 'subject_id', 'feed_type', 'category_id',
+    'publisher_type', 'publisher_id', 'city', 'score', 'reasons', 'is_exploration', 'shown_at',
+])]
 class RecommendationImpression extends Model
 {
     use HasStringPrimaryKey;
+
     public $incrementing = false;
     protected $keyType = 'string';
 
     protected function casts(): array
     {
-        return ['feed_type' => FeedType::class, 'score' => 'float', 'reasons' => 'array', 'shown_at' => 'datetime'];
+        return [
+            'feed_type' => FeedType::class,
+            'score' => 'float',
+            'reasons' => 'array',
+            'is_exploration' => 'boolean',
+            'shown_at' => 'datetime',
+        ];
     }
 
     public function user(): BelongsTo { return $this->belongsTo(User::class); }
