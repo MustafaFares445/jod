da3e5e7ef0b8da2f709dd098ad0b891553e14619
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-08 14:26:51 +0300

    Add email test endpoint configuration flag

--- config/mail.php
+++ config/mail.php
@@ -16,6 +16,8 @@ return [
 
     'default' => env('MAIL_MAILER', 'log'),
 
+    'test_endpoint_enabled' => (bool) env('MAIL_TEST_ENDPOINT_ENABLED', false),
+
     /*
     |--------------------------------------------------------------------------
     | Mailer Configurations
