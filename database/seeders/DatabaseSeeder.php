4ddcbd04e2e8d2134654102fd77dd43d4a59e167
Jawad Ghareeb <146003194+JawadGhareeb@users.noreply.github.com>
2026-09-09 00:11:16 +0300

    feat: include real Syrian videos in demo seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PersonalizationDemoSeeder::class);
         $this->call(StudentAssistanceSeedRefinementSeeder::class);
         $this->call(SyrianRealGroupsSeeder::class);
+        $this->call(SyrianRealVideosSeeder::class);
         $this->call(OrganizationRolePermissionSyncSeeder::class);
     }
 }
