2dea86a119d4aa32c655d78c7a3f067d0964fb4d
Jawad Ghareeb <146003194+JawadGhareeb@users.noreply.github.com>
2026-09-09 00:12:05 +0300

    test: skip external video downloads during test seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,11 @@ class DatabaseSeeder extends Seeder
         $this->call(PersonalizationDemoSeeder::class);
         $this->call(StudentAssistanceSeedRefinementSeeder::class);
         $this->call(SyrianRealGroupsSeeder::class);
-        $this->call(SyrianRealVideosSeeder::class);
+
+        if (! app()->environment('testing')) {
+            $this->call(SyrianRealVideosSeeder::class);
+        }
+
         $this->call(OrganizationRolePermissionSyncSeeder::class);
     }
 }
