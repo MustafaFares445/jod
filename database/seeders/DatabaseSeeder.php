74f99b82252bb567c3b2150813c7b68688e65819
Jawad Ghareeb <146003194+JawadGhareeb@users.noreply.github.com>
2026-09-09 09:28:43 +0300

    feat: include primary user journey in database seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,5 +28,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SeedOperationalIdentifiersSeeder::class);
         $this->call(SeedDataPresentationSeeder::class);
         $this->call(RestoreAdminAccessSeeder::class);
+        $this->call(PrimaryUserJourneySeeder::class);
     }
 }
